8553a2446cb6db4794e07cd01138b6dba17e6a59
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-01-07 20:41:42 +0530

    added styleFeature and ejs include template

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const path = require('path')
 const app = express()
 const port = 3000
 app.set("view engine","ejs")
 app.use(bodyparser.urlencoded({extended:true}))
-
+app.use(express.static('public'))
+// publicpath = path.join(__dirname,'/public')
 app.get('/',(req,res)=>{
     res.render("index")
 })
